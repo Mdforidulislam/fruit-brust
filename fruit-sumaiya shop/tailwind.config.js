2c66ec4b70857f818e9feb67a890eee6f2889608
Mdforidulislam <mdforidulislam962@gmail.com>
2023-07-26 22:14:32 +0600

    fix some problems

--- fruit-sumaiya shop/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const plugin = require('tailwindcss/plugin')
-
-module.exports = {
-  plugins: [
-    plugin(function({ addUtilities, addComponents, e, config }) {
-      // Add your custom styles here
-    }),
-  ]
-}
